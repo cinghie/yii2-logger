c003218fea5502767b12b0264f99d4ae5f807275
Giando <giando.olini@gogodigital.it>
2020-12-13 10:24:08 +0100

    Adding Module

---
+++ Logger.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-logger
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-logger
+ * @version 0.0.1
+ */
+
+namespace cinghie\logger;
+
+use yii\i18n\PhpMessageSource;
+
+/**
+ * Class Logger
+ */
+class Logger
+{
+	/**
+	 * @var array
+	 */
+	public $modelMap = [];
+
+	/**
+	 * @var string[]
+	 */
+	public $roles = ['admin'];
+
+	/**
+	 * @var bool
+	 */
+	public $showTitles = false;
+
+	/**
+	 * Carriers init
+	 */
+	public function init()
+	{
+		$this->registerTranslations();
+
+		parent::init();
+	}
+
+	/**
+	 * Translating module message
+	 */
+	public function registerTranslations()
+	{
+		if (!isset(Yii::$app->i18n->translations['logger*']))
+		{
+			Yii::$app->i18n->translations['logger*'] = [
+				'class' => PhpMessageSource::class,
+				'basePath' => '@vendor/cinghie/yii2-traits/messages',
+			];
+		}
+	}
+}
